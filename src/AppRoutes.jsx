000144a9c42fcc76dfe08b7c241c8f754770ea7b
medeiroscaio <caiovictor.medeiros50@gmail.com>
2024-08-29 09:56:06 -0300

    homepage initialized

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,17 +1,17 @@
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
-import Login from "./Login/Login.jsx"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Login from "./Login/Login.jsx";
+import Home from "./Home/Home.jsx";
 
 function AppRoutes() {
-    return (
-
-        <BrowserRouter>
-            <Routes>
-                <Route path="/Login" element={<Login/>}></Route>
-                <Route path="*" element={<Navigate to="/Login"/>}></Route>
-            </Routes>
-        </BrowserRouter>
-
-)
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Login" element={<Login />}></Route>
+        <Route path="/Home" element={<Home />}></Route>
+        <Route path="*" element={<Navigate to="/Login" />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default AppRoutes;
