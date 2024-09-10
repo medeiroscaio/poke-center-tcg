2b41fe27bd436228f59165a64d72428a0ae047e4
medeiroscaio <caiovictor.medeiros50@gmail.com>
2024-09-10 16:27:28 -0300

    import image base 64

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,33 +1,73 @@
 import "./Dropdown.css";
-import userProfile from "../../assets/user-profile.jpg"
+import userProfile from "../../assets/user-profile.jpg";
 import { CiImageOn } from "react-icons/ci";
 import { CiLogout } from "react-icons/ci";
+import { useState } from "react";
 
-function DropdownItem(props){
-    return(
-        <li className="dropdown-item">
-            {props.icon && <span>{props.icon}</span>}
-            <a> {props.text} </a>
-        </li>
-    );
-  }
+function DropdownItem(props) {
+  return (
+    <li className="dropdown-item">
+      {props.icon && <span>{props.icon}</span>}
+      {props.children}
+    </li>
+  );
+}
 
 const Dropdown = () => {
-    return (
-        <div className="dropdown-container">
-        <div className="dropdown-trigger">
-            <img src={userProfile} />
-        </div>
-        <div className="dropdown-menu">
-            <h3>Teste</h3>
-            <ul>
-                <DropdownItem icon={<CiImageOn />} text="Profile Image" />
-                <DropdownItem icon={<CiLogout />} text="Logout" />
-            </ul>
-        </div>
-        </div>
-    );
-};
+  const [imageProfile, SetimageProfile] = useState("");
 
+  function convertToBase64(e) {
+    const file = e.target.files[0];
+
+    if (!file) {
+      return;
+    }
+
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      SetimageProfile(reader.result);
+    };
+    reader.onerror = (error) => {
+      console.log("Error:", error);
+    };
+  }
+
+  const triggerFileUpload = () => {
+    document.getElementById("file-input").click();
+  };
+
+  return (
+    <div className="dropdown-container">
+      <div className="dropdown-trigger">
+        <img
+          src={
+            imageProfile === "" || imageProfile === null
+              ? userProfile
+              : imageProfile
+          }
+          alt="User profile"
+        />
+      </div>
+      <div className="dropdown-menu">
+        <h3>Teste</h3>
+        <ul>
+          <DropdownItem icon={<CiImageOn onClick={triggerFileUpload} />}>
+            <input
+              id="file-input"
+              type="file"
+              accept="image/*"
+              onChange={convertToBase64}
+              style={{ display: "none" }}
+            />
+          </DropdownItem>
+          <DropdownItem icon={<CiLogout />}>
+            <h1>Logout</h1>
+          </DropdownItem>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Dropdown;
