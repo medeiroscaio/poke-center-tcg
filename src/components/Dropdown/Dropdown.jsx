ca9fb77a5072557cc251c80b214892464d6ed4c8
svneto <severinoferreiracontato@gmail.com>
2024-09-10 16:27:13 -0300

    updated: dropdown now works

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -2,27 +2,36 @@ import "./Dropdown.css";
 import userProfile from "../../assets/user-profile.jpg"
 import { CiImageOn } from "react-icons/ci";
 import { CiLogout } from "react-icons/ci";
+import { useState } from "react";
 
-function DropdownItem(props){
-    return(
-        <li className="dropdown-item">
-            {props.icon && <span>{props.icon}</span>}
-            <a> {props.text} </a>
-        </li>
+function DropdownItem(props) {
+    return (
+      <li className="dropdown-item">
+        {props.icon && <span>{props.icon}</span>}
+        {props.children}
+      </li>
     );
   }
 
 const Dropdown = () => {
+
+    const [open, setOpen] = useState(false);
+
     return (
         <div className="dropdown-container">
-        <div className="dropdown-trigger">
+        <div className="dropdown-trigger" onClick={()=>{setOpen(!open)}}>
             <img src={userProfile} />
         </div>
-        <div className="dropdown-menu">
-            <h3>Teste</h3>
+        <div className={`dropdown-menu ${open? 'active' : 'inactive'}`}>
+            <h3>Teste<br/><span>Teste</span></h3>
+
             <ul>
-                <DropdownItem icon={<CiImageOn />} text="Profile Image" />
-                <DropdownItem icon={<CiLogout />} text="Logout" />
+                <DropdownItem icon={<CiImageOn />}>
+                
+                </DropdownItem>
+                <DropdownItem icon={<CiLogout />}>
+                <a>Log Out</a>
+                </DropdownItem>
             </ul>
         </div>
         </div>
