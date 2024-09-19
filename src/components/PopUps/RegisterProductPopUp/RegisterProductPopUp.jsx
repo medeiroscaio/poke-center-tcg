201f3089e8993b0946396daf238998079a2de05c
SilasMiguel <silas.works1@gmail.com>
2024-09-19 11:31:28 -0300

    Layout of register PopUp updated(css and placeholder logic)

--- src/components/PopUps/RegisterProductPopUp/RegisterProductPopUp.jsx
+++ src/components/PopUps/RegisterProductPopUp/RegisterProductPopUp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { IoClose } from "react-icons/io5";
 import "./RegisterProductPopUp.css";
 
@@ -8,7 +8,10 @@ const RegisterProductPopUp = ({ isOpen, onClose }) => {
   const [selectedCard, setSelectedCard] = useState(null);
   const [cards, setCards] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
-
+  const [tamanhoPlaceHolderContainer, setTamanhoPlaceHolderContainer] = useState(0);
+  
+  const cardsContainerRef = useRef(null);
+  
   const handleClose = () => {
     setProductQuantity(0);
     setProductPrice("");
@@ -109,73 +112,113 @@ const RegisterProductPopUp = ({ isOpen, onClose }) => {
     setSelectedCard(card);
   };
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (cardsContainerRef.current) {
+        const largura = cardsContainerRef.current.offsetWidth;
+        const altura = 0.8 * window.innerHeight;
+
+        const tamanhoCardContainer = largura * altura; //Tamanho quadrado² do container pai
+
+        const tamanhoImgCard = 150 * 200.52 //Tamanho do card da imagem
+
+        let tamanhoPlaceHolderContainerr = tamanhoCardContainer / tamanhoImgCard; //Quantiade de cards que cabem dentro da div pai
+
+        setTamanhoPlaceHolderContainer(tamanhoPlaceHolderContainerr) //Setando quantidade no state
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+  }, [isOpen, cards]);
+
   if (!isOpen) return null;
 
   return (
     <div className="popup-overlay">
       <div className="popup-content register">
-        <IoClose className="x-close" onClick={handleClose} />
+
+        <IoClose className="x-close" onClick={handleClose} />{/*Botão de fechar*/}
+
         <div className="popup-body">
+
           <div className="form-section">
             <h2>Registrar Produto</h2>
             <form onSubmit={handleSubmit}>
               <div className="form-group">
-                <label>Nome da Carta:</label>
-                <input
-                  type="text"
-                  id="search-input"
-                  onKeyDown={handleSearch}
-                  placeholder="Digite o nome da carta e aperte Enter"
-                />
-              </div>
-              <div className="form-group">
-                <label>Quantidade em Estoque:</label>
-                <input
-                  type="number"
-                  onChange={(e) => setProductQuantity(Number(e.target.value))}
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label>Preço do Produto:</label>
-                <input
-                  type="number"
-                  value={productPrice}
-                  onChange={(e) => setProductPrice(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="button-group">
-                <button type="submit">Registrar</button>
-              </div>
-              {errorMessage && (
-                <div className="error-message">{errorMessage}</div>
-              )}
-            </form>
-          </div>
-          <div className="cards-section">
-            <div className="cards-container">
-              {cards.map((card) => (
-                <div
-                  key={card.id}
-                  className={`card ${
-                    selectedCard?.id === card.id ? "selected" : ""
-                  }`}
-                  onClick={() => handleCardClick(card)}
-                >
-                  <img
-                    src={
-                      card.image
-                        ? `${card.image}/high.png`
-                        : "../src/assets/placeholder.png"
-                    }
-                    alt={card.name}
-                    className="card-image"
+
+                <div className="input-label">
+                  <label>Nome da Carta:</label>
+                  <input
+                    type="text"
+                    id="search-input"
+                    onKeyDown={handleSearch}
+                    placeholder="Digite o nome da carta e aperte Enter"
+                    />
+                </div>{/*input-label*/}
+        
+                <div className="input-label">
+                  <label>Quantidade em Estoque:</label>
+                  <input
+                    type="number"
+                    onChange={(e) => setProductQuantity(Number(e.target.value))}
+                    required
+                    />
+                </div>{/*input-label*/}
+            
+                <div className="input-label">
+                  <label>Preço do Produto:</label>
+                  <input
+                    type="number"
+                    value={productPrice}
+                    onChange={(e) => setProductPrice(e.target.value)}
+                    required
                   />
-                  <h2>{card.name}</h2>
-                  <p>ID: {card.id}</p>
-                </div>
-              ))}
+                </div>{/*input-label*/}
+                
+                <div className="button-group"> <button type="submit">Registrar</button> </div>
+
+              </div> {/*form-group*/}
+              
+              {errorMessage && (<div className="error-message">{errorMessage}</div>)}{/*FeedBack*/}
+
+            </form>
+          </div>{/*form-section*/}
+
+          <div className="cards-section" ref={cardsContainerRef}>
+            <div className="cards-container">
+              {cards.length === 0 ? (
+                /*If Ternário ===>>> PlaceHolder*/
+                Array.from({ length: parseInt(tamanhoPlaceHolderContainer / 1.5 )}).map(() => (
+                  <div className="card placeholder">
+                    <img
+                      src="../src/assets/placeholder.png"
+                      alt="placeholder"
+                      className="card-image"
+                    />
+                  </div>
+                ))
+              ) : (
+                /*Else do If Ternário ===>>> Cartas*/
+                cards.map((card) => (
+                  <div
+                    key={card.id}
+                    className={`card ${selectedCard?.id === card.id ? "selected" : ""}`}
+                    onClick={() => handleCardClick(card)}
+                    >
+                    <img
+                      src={
+                        card.image
+                          ? `${card.image}/high.png`
+                          : "../src/assets/placeholder.png"
+                      }
+                      alt={card.name}
+                      className="card-image"
+                    />
+                    <h2>{card.name}</h2>
+                    <p>ID: {card.id}</p>
+                  </div>
+                ))
+              )}
             </div>
           </div>
         </div>
